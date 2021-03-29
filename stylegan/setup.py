3406c207fe50d5ee549d48ea6d3fe804752a88cb
Britt0508 <britt2609@hotmail.com>
2021-03-29 11:07:01 +0200

    added setup.py

---
+++ stylegan/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='ExplainedKinshipCorrect',
+    version='3.14',
+    packages=['dnnlib', 'dnnlib.tflib', 'dnnlib.submission', 'metrics', 'training', 'tflib', 'submission'],
+    package_dir={'': 'stylegan'},
+    url='',
+    license='',
+    author='Gebruiker',
+    author_email='b.e.van.leeuwen@student.vu.nl',
+    description='Setup for colab'
+)
