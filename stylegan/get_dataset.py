a54009a00f60c8461c06be0a1fe9d495dad71822
Britt0508 <britt2609@hotmail.com>
2021-05-10 13:40:29 +0200

    dataset

--- stylegan/get_dataset.py
+++ stylegan/get_dataset.py
@@ -1,9 +1,39 @@
 import csv
 import pandas as pd
 from stylegan.metrics import linear_separability
+from collections import defaultdict
+from glob import glob
+from random import choice, sample
 
 
 def get_data():
+    train_file_path = "../content/drive/MyDrive/ExplainedKinshipData/data/train_pairs.csv"
+    train_folders_path = "../data/train-faces/"
+    val_families = "F09"
+
+    all_images = glob(train_folders_path + "*/*/*.jpg")
+
+    train_images = [x for x in all_images if val_families not in x]
+    val_images = [x for x in all_images if val_families in x]
+
+    train_person_to_images_map = defaultdict(list)
+
+    ppl = [x.split("/")[-3] + "/" + x.split("/")[-2] for x in all_images]
+
+    for x in train_images:
+        train_person_to_images_map[x.split("/")[-3] + "/" + x.split("/")[-2]].append(x)
+
+    val_person_to_images_map = defaultdict(list)
+
+    for x in val_images:
+        val_person_to_images_map[x.split("/")[-3] + "/" + x.split("/")[-2]].append(x)
+
+    relationships = pd.read_csv(train_file_path)
+    relationships = list(zip(relationships.p1.values, relationships.p2.values))
+    relationships = [x for x in relationships if x[0] in ppl and x[1] in ppl]
+
+    train = [x for x in relationships if val_families not in x[0]]
+    val = [x for x in relationships if val_families in x[0]]
     df_pairs = pd.read_csv("/content/drive/MyDrive/ExplainedKinshipData/data/train-pairs.csv")
     print(df_pairs)
     features = []
