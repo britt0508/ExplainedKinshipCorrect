6a13c8ca332ef248dafc050687262663a0079f53
Britt0508 <britt2609@hotmail.com>
2021-05-18 12:17:42 +0200

    dataset code

---
+++ stylegan/get_data_2.py
@@ -0,0 +1,97 @@
+from torch.utils.data import Dataset, DataLoader
+from torchvision import transforms as T
+from PIL import Image
+import os
+import csv
+
+
+class FIW_DBBase(Dataset):
+    def __init__(self, root_dir, labels_path, n_classes=300, transform=None):
+        self.root_dir = root_dir
+        self.labels_path = labels_path
+        self.n_classes = n_classes
+        self.transform = transform
+        self.pairs = []
+
+    def __len__(self):
+        return len(self.pairs)
+
+    def __getitem__(self, idx):
+        pass
+
+
+
+class FIW_Train(FIW_DBBase):
+    """Dataset class for FIW Training Set"""
+
+    def preprocess(self):
+        """Process the labels file"""
+        lines = [line.rstrip() for line in open(self.labels_path, 'r')]
+
+        for l in lines:
+            spt = l.split()
+            fname = spt[0]
+            label = int(spt[1])
+            # label_vec = [1 if i == label else 0 for i in range(self.n_classes)]
+            self.pairs.append([fname, label])
+
+    def __getitem__(self, index):
+        """Return an image"""
+        filename, label = self.pairs[index]
+        impath = os.path.join(self.root_dir, filename)
+        image = Image.open(impath)
+        return self.transform(image), label
+
+class FIW_Val(FIW_DBBase):
+    """Dataset class for FIW Validation Set"""
+
+    def preprocess(self):
+        """Process the pair CSVs"""
+        with open(self.labels_path, 'r') as f:
+            re = csv.reader(f)
+            lines = list(re)
+
+        self.pairs = [(l[2], l[3], bool(int(l[1]))) for l in lines]
+
+    def __getitem__(self, index):
+        """Return a pair"""
+        path_a, path_b, label = self.pairs[index]
+        img_a = self.transform(Image.open(os.path.join(self.root_dir, path_a)))
+        img_b = self.transform(Image.open(os.path.join(self.root_dir, path_b)))
+
+        return (img_a, img_b), label
+
+    def __len__(self):
+        """Return the number of images."""
+
+        return len(self.pairs)
+
+    def get_train_loader(image_dir, labels_path='train/train.label', n_classes=300, image_size=(112, 96),
+                         batch_size=16,
+                         num_workers=1):
+        """Build and return a data loader for the training set."""
+        transform = T.Compose([T.RandomHorizontalFlip(),
+                               T.Resize(image_size),
+                               T.ToTensor(),
+                               T.Normalize(mean=(0.5, 0.5, 0.5), std=(0.5, 0.5, 0.5))
+                               ])
+        dataset = FIW_Train(image_dir, labels_path, n_classes=n_classes, transform=transform)
+        dataset.preprocess()
+        data_loader = DataLoader(dataset=dataset, batch_size=batch_size,
+                              shuffle=True,
+                              num_workers=num_workers)
+        return data_loader
+
+    def get_val_loader(base_dir, csv_path, image_size=(112, 96), batch_size=128, num_workers=1):
+        """Build and return a data loader for a split in the validation set."""
+        transform = T.Compose([T.Resize(image_size),
+                               T.ToTensor(),
+                               T.Normalize(mean=(0.5, 0.5, 0.5), std=(0.5, 0.5, 0.5))
+                               ])
+
+        dataset = FIW_Val(base_dir, csv_path, transform=transform)
+        dataset.preprocess()
+        data_loader = DataLoader(dataset=dataset, batch_size=batch_size,
+                                  shuffle=False,
+                                  num_workers=num_workers)
+        return data_loader
--- stylegan/get_dataset.py
+++ stylegan/get_dataset.py
@@ -1,14 +1,14 @@
 import csv
 import pandas as pd
-from stylegan.metrics import linear_separability
+# from stylegan.metrics import linear_separability
 from collections import defaultdict
 from glob import glob
 from random import choice, sample
 
 
 def get_data():
-    train_file_path = "../content/drive/MyDrive/ExplainedKinshipData/data/train_pairs.csv"
-    train_folders_path = "../data/train-faces/"
+    train_file_path = "/content/drive/MyDrive/ExplainedKinshipData/data/train-pairs.csv"
+    train_folders_path = "/content/drive/MyDrive/ExplainedKinshipData/data/train-faces/"
     val_families = "F09"
 
     all_images = glob(train_folders_path + "*/*/*.jpg")
@@ -34,14 +34,51 @@ def get_data():
 
     train = [x for x in relationships if val_families not in x[0]]
     val = [x for x in relationships if val_families in x[0]]
-    df_pairs = pd.read_csv("/content/drive/MyDrive/ExplainedKinshipData/data/train-pairs.csv")
-    print(df_pairs)
-    features = []
-    for pair in df_pairs:
-        current_features_1 = linear_separability.get_features(pair["p1"], pair["ptype"])
-        current_features_2 = linear_separability.get_features(pair["p2"], pair["ptype"])
-        features.append([current_features_1, current_features_2])
 
-    df_pairs.insert(-1, "features", features, True)
-    print(df_pairs)
-    return df_pairs
+    print(relationships)
+    print(train)
+
+    gen(train, train_person_to_images_map, batch_size=16)
+
+
+def gen(list_tuples, person_to_images_map, batch_size=16):
+    ppl = list(person_to_images_map.keys())
+    while True:
+        batch_tuples = sample(list_tuples, batch_size // 2)
+        labels = [1] * len(batch_tuples)
+        while len(batch_tuples) < batch_size:
+            p1 = choice(ppl)
+            p2 = choice(ppl)
+
+            if p1 != p2 and (p1, p2) not in list_tuples and (p2, p1) not in list_tuples:
+                batch_tuples.append((p1, p2))
+                labels.append(0)
+
+        for x in batch_tuples:
+            if not len(person_to_images_map[x[0]]):
+                print(x[0])
+
+        X1 = [choice(person_to_images_map[x[0]]) for x in batch_tuples]
+        X1 = np.array([read_img(x) for x in X1])
+
+        X2 = [choice(person_to_images_map[x[1]]) for x in batch_tuples]
+        X2 = np.array([read_img(x) for x in X2])
+
+        print(X1, X2, labels)
+        yield [X1, X2], labels
+
+    # df_pairs = pd.read_csv("/content/drive/MyDrive/ExplainedKinshipData/data/train-pairs.csv")
+    # print(df_pairs)
+
+    # features = []
+    # for pair in df_pairs:
+    #     current_features_1 = linear_separability.get_features(pair["p1"], pair["ptype"])
+    #     current_features_2 = linear_separability.get_features(pair["p2"], pair["ptype"])
+    #     features.append([current_features_1, current_features_2])
+
+    # df_pairs.insert(-1, "features", features, True)
+    # print(df_pairs)
+    # return df_pairs
+
+
+get_data()
