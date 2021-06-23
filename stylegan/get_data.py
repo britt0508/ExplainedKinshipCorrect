ed76c23756ed45955e2416aaf340fdeeabec8636
Britt0508 <britt2609@hotmail.com>
2021-06-23 16:25:52 +0200

    all data ready, first models coded. Still need to add nonfam pairs

--- stylegan/get_data.py
+++ stylegan/get_data.py
@@ -1,26 +1,31 @@
-from torch.utils import data
-from torchvision import transforms as T
-from torchvision.datasets import ImageFolder
-import torch
-import torchvision
-from PIL import Image
-import torch
+import tensorflow as tf
+
+import pandas as pd
 import os
 import random
 import csv
 import glob
-import tensorflow as tf
-from stylegan.metrics import linear_separability
-import pandas as pd
+import argparse
+import numpy as np
 
-BASE_PATH = "/content/drive/MyDrive/ExplainedKinshipData/data/"
-LABELS_PATH = os.path.join(BASE_PATH, "train-pairs.csv")
-IMAGE_PATH = os.path.join(BASE_PATH, "train-faces/")
-IMAGE_PATH_ARWIN = "C:/Users/Arwin/PycharmProjects/ExplainedKinshipCorrect/data/train-faces"
-DATA_PATH = os.path.join(BASE_PATH, 'pic_train_pairs.csv')
-LOADER_PATH = os.path.join(BASE_PATH, 'loader_pic_train_pairs.csv')
-FEAT_PATH = os.path.join(BASE_PATH, "features_all")
-FEAT_SMALL_PATH = os.path.join(BASE_PATH, "features_all")
+import numpy as np
+import sklearn.svm
+import pickle
+import cv2
+
+from PIL import Image
+from pathlib import Path
+from collections import defaultdict
+
+import stylegan.dnnlib as dnnlib
+import stylegan.dnnlib.tflib as tflib
+
+from stylegan import config
+
+import sys
+
+# tf.compat.v1.enable_eager_execution()
+
 
 classifier_urls = [
     'https://nvlabs-fi-cdn.nvidia.com/stylegan/networks/metrics/celebahq-classifier-00-male.pkl',
@@ -67,54 +72,100 @@ classifier_urls = [
 no_urls = len(classifier_urls)
 
 
-def write_features_csv():
-    print("just get me those features man")
-    # with open(FEAT_PATH, mode='w') as feat_csv:
-    #     feat_csv = csv.writer(feat_csv, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-    #     feat_csv.writerow(["pic_path", "features"])
+def open_file_or_url(file_or_url):
+    if dnnlib.util.is_url(file_or_url):
+        return dnnlib.util.open_url(file_or_url, cache_dir=config.cache_dir)
+    return open(file_or_url, 'rb')
+
+
+def load_pkl(file_or_url):
+    with open_file_or_url(file_or_url) as file:
+        dnnlib.tflib.init_tf()
+        return pickle.load(file, encoding='latin1')
+
+
+# Get classifier for features once
+def get_features(imgs, predictions, x):
+    all_images = []
+    for image_input in imgs:
+        image = cv2.imread(str(image_input))
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        image = image - np.mean(image, (0, 1))
+        image = image / np.std(image, axis=(0, 1))
+        image = cv2.resize(image, (256, 256), interpolation=cv2.INTER_AREA)
+        image = np.array(image)
+        image = np.expand_dims(image.transpose((2, 0, 1)), axis=0)
+        all_images.append(image)
+    concat_imgs = np.concatenate(all_images)
+
+    result = tflib.run(predictions, feed_dict={x: concat_imgs})[0]
+
+    return result[:, 1].tolist()
+
+
+def write_features_csv(args):
+    # tflib.init_tf()
+    train_adjective = 'test'
+    BASE_PATH = args.data_dir
+    # LABELS_PATH = os.path.join(BASE_PATH, "train-pairs.csv")
+    IMAGE_PATH = os.path.join(BASE_PATH, "{}-faces/".format(train_adjective))
+    # DATA_PATH = os.path.join(BASE_PATH, 'pic_train_pairs.csv')
+    # LOADER_PATH = os.path.join(BASE_PATH, 'loader_pic_train_pairs.csv')
+    FEAT_PATH = os.path.join(args.out_dir, "features_all_{}_{}.csv".format(train_adjective, str(args.feature)))
+    FEAT_PATH_TEST = os.path.join(args.out_dir, "features_all_{}_{}.csv".format(train_adjective, str(args.feature)))
 
     # Get list of all image paths
     im_path = []
-    for img in glob.glob(IMAGE_PATH + "***/**/*.jpg"):
+    im_path_rel = []
+    for img in sorted(glob.glob(IMAGE_PATH + "*.jpg")):
         im_path.append(img)
+        im_path_rel.append(os.path.relpath(img, IMAGE_PATH))
+    # im_path = im_path # remove for full dataset
     print("image paths loaded")
 
     # Get feature values per feature for each picture:
     # [[feat1], [feat2], ..., [feat40]] with [feat1] a list of feature 1 values for all pictures
-    features = []
-    count2 = 0
-    for url in classifier_urls:
-        classifier = linear_separability.load_pkl(url)
-        count = 0
-        row = []
-        batch_size = 128
-        im_len = len(im_path)
-        for i in range(0, im_len, batch_size):
-            if im_len > i+batch_size:
-                imgs = im_path[i:i+batch_size]
-            else:
-                imgs = im_path[i:im_len]
-            feature = linear_separability.get_features(imgs, classifier)
-            row.append(feature)
-            count += 1
-            print(count)
-        count2 += 1
-        print(count2)
-        print(feature)
+    batch_size = args.batch_size
+    x = tf.placeholder(tf.float32, shape=(None, 3, 256, 256))
 
-        features.append(row)
-        print("classifier loaded: " + str(url))
-    df = pd.DataFrame([im_path + features], column=["Image_path"] + [str(i) for i in no_urls])
-    df.to_csv(FEAT_PATH)
+    url = classifier_urls[args.feature]
+    c = load_pkl(url)
+    logits = c.get_output_for(x, None, is_validation=True, randomize_noise=False, structure='fixed')
+    predictions = [tf.nn.softmax(tf.concat([logits, -logits], axis=1))]
+
+    row = []
+
+    print("Performing feature: " + str(args.feature))
+    im_len = len(im_path)
+    for i in range(0, im_len, batch_size):
+        print("Performing batch: " + str(int(i / batch_size)))
+        # tf.reset_default_graph()
+        # classifier = load_pkl(url)
+        if im_len > i + batch_size:
+            imgs = im_path[i:i + batch_size]
+        else:
+            imgs = im_path[i:im_len]
+        feature = get_features(imgs, predictions, x)
+        row.extend(feature)
+
+        if int(i / batch_size) % 20 == 0:
+            df = pd.DataFrame(np.array([im_path_rel[:len(row)]] + [row]).T,
+                              columns=["Image_path"] + [args.feature])
+            df.to_csv(FEAT_PATH_TEST)
+            df = None
+
+    print("classifier loaded: " + str(url))
+    df = pd.DataFrame(np.array([im_path_rel[:len(row)]] + [row]).T, columns=["Image_path"] + [args.feature])
+    df.to_csv(FEAT_PATH_TEST)
     return df
 
 
-def write_features_pairs_csv():
+def write_features_pairs_csv(args):
     with open(LABELS_PATH) as csv_file:
         pairs_data = csv.reader(csv_file)
         next(pairs_data, None)
 
-        all_features = pd.read_csv(FEAT_SMALL_PATH)
+        all_features = pd.read_csv(FEAT_PATH)
         # all_features = write_features_csv()
 
         with open(str(BASE_PATH) + 'pic_train_pairs.csv', mode='w') as pic_csv:
@@ -122,40 +173,96 @@ def write_features_pairs_csv():
             pic_train_writer.writerow(["pic1", "pic2", "p1", "p2", "ptype"])
 
             for pair in pairs_data:
-                # print(pair)
+                print(pair)
                 for picture_pair1 in os.listdir(str(IMAGE_PATH) + str(pair[0])):
-                    pic_path1 = str(pair[0]) + "/" + str(picture_pair1)
-                    # pic_1_entire_path = str(IMAGE_PATH) + str(pic_path1)
-                    pic_1_arwin_path = str(IMAGE_PATH_ARWIN) + "/" + str(pic_path1)
+                    for picture_pair2 in os.listdir(str(IMAGE_PATH) + str(pair[1])):
+                        # Look at each picture separately
+                        pic_path1 = str(pair[0]) + "/" + str(picture_pair1)
+                        pic_path2 = str(pair[1]) + "/" + str(picture_pair2)
+                        pic_1_entire_path = str(IMAGE_PATH) + str(pic_path1)
+                        pic_2_entire_path = str(IMAGE_PATH) + str(pic_path2)
 
-                    if pic_1_arwin_path in all_features:
-                        print("pic 1 path yes")
-                        features_1 = all_features.loc[all_features['Image_path'] == pic_1_arwin_path]
+                        # Get features from stylegan lin sep per picture
+                        features_1 = all_features.loc[all_features['Image_path'] == pic_1_entire_path]
+                        features_2 = all_features.loc[all_features['Image_path'] == pic_2_entire_path]
 
-                        for picture_pair2 in os.listdir(str(IMAGE_PATH) + str(pair[1])):
-                            # Look at each picture separately
-                            pic_2_arwin_path = str(IMAGE_PATH_ARWIN) + str(pic_path2)
-                            if pic_2_arwin_path in all_features:
-                                print("pic 2 path yes")
-                                pic_path2 = str(pair[1]) + "/" + str(picture_pair2)
-                                # pic_2_entire_path = str(IMAGE_PATH) + str(pic_path2)
-
-                                # Get features from stylegan lin sep per picture
-                                features_2 = all_features.loc[all_features['Image_path'] == pic_2_arwin_path]
-
-                                # features_1 = linear_separability.get_features(str(IMAGE_PATH) + str(pic_path1))
-                                # features_2 = linear_separability.get_features(str(IMAGE_PATH) + str(pic_path2))
-
-                                # print(features_1)
-                                row = [pic_path1, pic_path2, pair[0], pair[1], pair[2], features_1, features_2]
-                                pic_train_writer.writerow(row)
+                        row = [pic_path1, pic_path2, pair[0], pair[1], pair[2], features_1, features_2]
+                        pic_train_writer.writerow(row)
             print(pic_train_writer)
             pic_csv.close()
 
 
-# write_features_csv()
-write_features_pairs_csv()
+def write_features_pairs_csv_test(all_features):
+    BASE_PATH = "/content/drive/MyDrive/ExplainedKinshipData/data/"
+    LABELS_PATH = os.path.join(BASE_PATH, "test_competition.xlsx")
+    IMAGE_PATH = os.path.join(BASE_PATH, "test-faces/")
+
+    # with open(LABELS_PATH) as csv_file:
+    df_pd = pd.read_excel(LABELS_PATH, names=["index", "first", "second", "relation"])
+    df_pd["features_1"] = np.nan
+    df_pd["features_2"] = np.nan
+    pd.set_option("display.max_columns", None)
+    print(df_pd)
+
+    for index, pair in df_pd.iterrows():
+        pic_path1 = pair["first"]
+        pic_1_entire_path = str(IMAGE_PATH) + str(pic_path1)
+        features_1 = all_features.loc[all_features['Image_path'] == pic_path1].values.tolist()[0][3:]
+
+        pic_path2 = pair["second"]
+        pic_2_entire_path = str(IMAGE_PATH) + str(pic_path2)
+        features_2 = all_features.loc[all_features['Image_path'] == pic_path2].values.tolist()[0][3:]
+
+        names1 = {pair["index"]: features_1}
+        names2 = {pair["index"]: features_2}
+
+        df_pd['features_1'] = df_pd[pair["index"]].map(names1)
+        df_pd['features_2'] = df_pd[pair["index"]].map(names2)
 
 
+def get_random_pairs_nonfam():
+    print("help")
 
+
+def str2bool(v):
+    if isinstance(v, bool):
+        return v
+    if v.lower() in ('yes', 'true', 't', 'y', '1'):
+        return True
+    elif v.lower() in ('no', 'false', 'f', 'n', '0'):
+        return False
+    else:
+        raise argparse.ArgumentTypeError('Boolean value expected.')
+
+
+# parser = argparse.ArgumentParser(description='Get a single feature for the entire dataset.')
+# parser.add_argument('--feature', type=int, help='The feature we extract in this run.', default=0)
+# parser.add_argument('--batch-size', type=int, help='Batch size used.', default=32)
+# parser.add_argument('--data-dir', type=str, help="Directory of the dataset.", required=True)
+# parser.add_argument('--out-dir', type=str, help="Output Directory", required=True)
+# parser.add_argument('--train', type=str2bool, help='Boolean whether to take training or validation images.',
+#                     default=True)
+
+# args = parser.parse_args()
+# write_features_csv(args)
+# write_features_pairs_csv()
+
+# path = "/content/drive/MyDrive/ExplainedKinshipData/data/Features_test/merged.csv"
+# with open(path) as csv_file:
+#     data = pd.read_csv(csv_file)
+#     write_features_pairs_csv_test(data)
+
+# path = "/content/drive/MyDrive/ExplainedKinshipData/data/Features_test"
+# all_files = glob.glob(os.path.join(path, "features_all_test_*.csv"))
+# combined_csv_data = pd.read_csv(all_files[0], delimiter=',')
+
+# for f in all_files[1:]:
+#     df = pd.read_csv(f, delimiter=',')
+#     df.set_index('Image_path', inplace=True)
+#     df = df.iloc[: , 1:]
+#     print(df)
+#     combined_csv_data = pd.merge(combined_csv_data, df, on='Image_path')
+
+# combined_csv_data.to_csv(os.path.join(path, "merged.csv"))
+# print(combined_csv_data)
 
--- stylegan/models.py
+++ stylegan/models.py
@@ -1,15 +1,291 @@
 import numpy as np
-from sklearn.linear_model import LinearRegression
 import csv
+import pandas as pd
+import matplotlib.pyplot as plt
 
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import GaussianNB, CategoricalNB
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report, confusion_matrix, accuracy_score, roc_curve, roc_auc_score, \
+    recall_score, precision_score
+from sklearn.preprocessing import LabelEncoder
+from sklearn.tree import export_graphviz
+from sklearn import metrics
+from sklearn import preprocessing
+from sklearn import tree
+
+from sklearn.svm import LinearSVC
+from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import StandardScaler
+from sklearn.datasets import make_classification
+
+# for data and modeling
+import keras
+from keras.callbacks import EarlyStopping
+from keras.wrappers.scikit_learn import KerasClassifier
+from keras.utils import np_utils
+from keras.models import Sequential
+from keras.layers import Dense, Dropout
+from keras.callbacks import EarlyStopping
+from tensorflow.keras import datasets, layers, models
+
+from six import StringIO
+from IPython.display import Image
+import pydotplus
+
+from ast import literal_eval
 
 DATA_PATH = "/content/drive/MyDrive/ExplainedKinshipData/data/pic_train_pairs.csv"
-with open(DATA_PATH) as csv:
-    x = "input x"
-    y = "input y"
+data = pd.read_csv(DATA_PATH, header=0,
+                   names=["pic1", "pic2", "p1", "p2", "ptype", "feat1", "feat2"],
+                   converters={"feat1": literal_eval, "feat2": literal_eval})
 
 
-def LinRegression():
-    model = LinRegression().fit(x, y)
+def NaiveBayes(x, y, xtest, ytest):
+    cnb = CategoricalNB()
+    cnb.fit(x, y)
+    cnb.predict(xtest)
+    y_pred_cnb = cnb.predict(X_test)
+    y_prob_pred_cnb = cnb.predict_proba(X_test)
+    # how did our model perform?
+    count_misclassified = (y_test != y_pred_cnb).sum()
+
+    print("CategoricalNB")
+    print("=" * 30)
+    print('Misclassified samples: {}'.format(count_misclassified))
+    accuracy = accuracy_score(y_test, y_pred_cnb)
+    print('Accuracy: {:.2f}'.format(accuracy))
+
+    # print("Number of mislabeled points out of a total %d points : %d" % (X_test.shape[0], (y_test != y_pred).sum()))
+
+
+def LinRegression(x, y):
+    model = LinearRegression().fit(x, y)
     r_sq = model.score(x, y)
     y_pred = model.predict(x)
+    print(r_sq)
+
+
+def DecisionTree(x, y, xtest, ytest):
+    clf = DecisionTreeClassifier(max_depth=5)
+
+    # Train Decision Tree Classifer
+    clf = clf.fit(x, y)
+
+    # Predict the response for test dataset
+    y_pred = clf.predict(xtest)
+    print("Accuracy:", metrics.accuracy_score(ytest, y_pred))
+    VisualizationTree(clf)
+
+
+def VisualizationTree(clf):
+    feature_cols = [i for i in range(80)]
+    # feature_names = df.columns[:5]
+    target_names = data['ptype'].unique().tolist()
+
+    tree.plot_tree(clf,
+                   feature_names=feature_cols,
+                   class_names=target_names,
+                   filled=True,
+                   rounded=True)
+
+    plt.savefig('tree_visualization.pdf')
+    # dot_data = StringIO()
+    # export_graphviz(clf, out_file=dot_data,
+    #                 filled=True, rounded=True,
+    #                 special_characters=True,feature_names = feature_cols,class_names=['0','1'])
+    # graph = pydotplus.graph_from_dot_data(dot_data.getvalue())
+    # graph.write_png('diabetes.png')
+    # Image(graph.create_png())
+
+
+def NeuralNetwork(X, y, xtest, ytest, feed_forward=False):
+    encoder = LabelEncoder()
+    encoder.fit(y)
+    encoded_Y = encoder.transform(y)
+    # convert integers to dummy variables (i.e. one hot encoded)
+    dummy_y = np_utils.to_categorical(encoded_Y)
+    model = Sequential()
+    if feed_forward:
+        model.add(Dense(256, input_shape=(784,), activation="sigmoid"))
+        model.add(Dense(128, activation="sigmoid"))
+        model.add(Dense(10, activation="softmax"))
+        model.summary()
+    else:
+        model.add(Dense(16, input_shape=(X.shape[1],), activation='relu'))  # input shape is (features,)
+        model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(32, 32, 3)))
+        model.add(layers.MaxPooling2D((2, 2)))
+        model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+        model.add(layers.MaxPooling2D((2, 2)))
+        model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+        model.add(Dense(11, activation='softmax'))
+        model.summary()
+
+    # compile the model
+    model.compile(optimizer='rmsprop',
+                  loss='categorical_crossentropy',
+                  # this is different instead of binary_crossentropy (for regular classification)
+                  metrics=['accuracy'])
+
+    # early stopping callback
+    # This callback will stop the training when there is no improvement in
+    # the validation loss for 10 consecutive epochs.
+    es = keras.callbacks.EarlyStopping(monitor='val_loss',
+                                       mode='min',
+                                       patience=10,
+                                       restore_best_weights=True)  # important - otherwise you just return the last weigths...
+
+    # now we just update our model fit call
+    history = model.fit(X,
+                        dummy_y,
+                        callbacks=[es],
+                        epochs=8000000,  # you can set this to a big number!
+                        batch_size=10,
+                        shuffle=True,
+                        validation_split=0.2,
+                        verbose=1)
+
+    history_dict = history.history
+
+    # learning curve
+    # accuracy
+    acc = history_dict['accuracy']
+    val_acc = history_dict['val_accuracy']
+
+    # loss
+    loss = history_dict['loss']
+    val_loss = history_dict['val_loss']
+
+    # range of X (no. of epochs)
+    epochs = range(1, len(acc) + 1)
+
+    # plot
+    # "r" is for "solid red line"
+    plt.plot(epochs, acc, 'r', label='Training accuracy')
+    # b is for "solid blue line"
+    plt.plot(epochs, val_acc, 'b', label='Validation accuracy')
+    plt.title('Training and validation accuracy')
+    plt.xlabel('Epochs')
+    plt.ylabel('Accuracy')
+    plt.legend()
+
+    plt.show()
+
+    preds = model.predict(X)  # see how the model did!
+    print(preds[0])  # i'm spreading that prediction across three nodes and they sum to 1
+    print(np.sum(preds[0]))  # sum it up! Should be 1
+    ## [9.9999988e-01 1.3509347e-07 6.7064638e-16]
+    ## 1.0
+
+    # Almost a perfect prediction
+    # actual is left, predicted is top
+    # names can be found by inspecting Y
+    matrix = confusion_matrix(dummy_y.argmax(axis=1), preds.argmax(axis=1))
+    print(matrix)
+
+    # more detail on how well things were predicted
+    print(classification_report(dummy_y.argmax(axis=1), preds.argmax(axis=1)))
+
+
+def SVM(X, y, xtest, ytest):
+    clf = make_pipeline(StandardScaler(), LinearSVC(random_state=0, tol=1e-5, multi_class="crammer_singer"))
+    clf.fit(X, y)
+    # print(clf.named_steps['linearsvc'].coef_)
+    # print(clf.named_steps['linearsvc'].intercept_)
+    ypred = clf.predict(xtest)
+    print(confusion_matrix(ytest, ypred))
+    print(classification_report(ytest, ypred))
+
+
+def Get_xy(one_hot=False):
+    data["feat1and2"] = data["feat1"] + data["feat2"]
+    f = data["feat1and2"]
+
+    classes = data["ptype"].values
+
+    if one_hot:
+        le = preprocessing.LabelEncoder()
+        le.fit(["sibs", "bb", "ss", "ms", "md", "fs", "fd", "gfgd", "gfgs", "gmgd", "gmgs"])
+        classes = le.transform(data["ptype"])
+
+    X_train, X_test, y_train, y_test = train_test_split(f, classes, test_size=0.3, random_state=0)
+
+    print("Data split")
+    # DO SOMETHING HERE WITH DATA TO MAKE IT WORK
+    X_train = np.array(list(map(list, X_train)))
+    # print(X_train)
+    y_train = np.squeeze(np.array(list(y_train)))
+
+    X_test = np.array(list(map(list, X_test)))
+    # print(X_test)
+    y_test = np.squeeze(np.array(list(y_test)))
+
+    # print(y_train)
+    # print(y_train.shape)
+    # print(X_train.shape)
+
+    return X_train, y_train, X_test, y_test
+
+
+X_train, y_train, X_test, y_test = Get_xy()
+# X_train, y_train, X_test, y_test = Get_xy(one_hot = True)
+
+# NeuralNetwork(X_train, y_train, X_test, y_test, feed_forward=True)
+DecisionTree(X_train, y_train, X_test, y_test)
+# NaiveBayes(X_train, y_train, X_test, y_test)
+# SVM(X_train, y_train, X_test, y_test)
+
+
+# features = data[["feat1", "feat2"]]
+# print(x)
+# features = x.extend(y)
+
+
+# DATA_PATH_TEST = "/content/drive/MyDrive/ExplainedKinshipData/data/merged.csv"
+# # LinRegression(features, ptypes)
+# test_data = pd.read_csv(DATA_PATH_TEST)
+# x_test = test_data["feature_1"].tolist()
+# y_test = test_data["feature_2"].tolist()
+# features_test = x_test.extend(y_test)
+# ptypes_test = test_data["ptype"].tolist()
+
+# Treee(features, ptypes, features_test, ptypes_test)
+
+# data["feat1"] = data.feat1.apply(literal_eval)
+# data["feat2"] = data.feat2.apply(literal_eval)
+
+# f1 = data["feat1"].tolist()
+# f2 = data["feat2"].tolist()
+# print(f1)
+# print(f2)
+
+# f = data[["feat1", "feat2"]].values
+# print(f)
+# f = data.drop(["pic1", "pic2", "p1", "p2", "ptype"], axis=1).values
+# print(f)
+# data["feat1and2"] = data["feat1"] + data["feat2"]
+# f = data["feat1and2"]
+
+# le = preprocessing.LabelEncoder()
+# le.fit(["ms", "md", "fs", "fd", "sibs", "bb", "ss", "gfgd", "gfgs", "gmgd", "gmgs"])
+# values = le.transform(data["ptype"])
+# X_train, X_test, y_train, y_test = train_test_split(f, values, test_size=0.3, random_state=1)
+
+# Get one hot encoding of columns ptype
+# one_hot = pd.get_dummies(data['ptype'])
+# one_hot['combined']= one_hot.values.tolist()
+# values = one_hot["combined"].values
+
+# ptypes = data["ptype"].tolist()
+# first_gen = ["ms", "md", "fs", "fd"]
+# zero_gen = ["sibs", "bb", "ss"]
+# ptypes_bin = [1 if i in first_gen else 0 if i in second_gen else 2 for i in ptypes]
+# print(ptypes_bin)
+# tv = data["ptype"].values # Target variable
+
+
+
+
+
